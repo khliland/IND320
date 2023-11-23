9766858670dadc80055a6ce229583a1b2653b9d6
Kristian Hovde Liland <khliland@users.noreply.github.com>
2023-11-23 10:00:13 +0100

    Compulsory part 4

--- streamsync/colours/main.py
+++ streamsync/colours/main.py
@@ -15,13 +15,32 @@ def _update_message(state):
 
 def decrement(state):
     state["counter"] -= 1
+    if state["counter"] >= 30:
+        state["too_high"] = True
+    else:
+        state["too_high"] = False
     _update_message(state)
 
 def increment(state):
     state["counter"] += 1
     # Shows in the log when the event handler is run
-    print(f"The counter has been incremented.")
+    if state["counter"] >= 30:
+        state["too_high"] = True
     _update_message(state)
+
+# Create a matlibplot figure with a line plot
+def plot(state):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    x = np.linspace(0, 2 * np.pi, 100)
+    y = np.sin(x)
+
+    fig = plt.figure()
+    plt.plot(x, y)
+    plt.close(fig)
+
+    state["plot"] = fig
     
 # Initialise the state
 
@@ -35,6 +54,8 @@ initial_state = ss.init_state({
     "_my_private_element": 1337,
     "message": None,
     "counter": 26,
+    "too_high":False,
 })
 
 _update_message(initial_state)
+plot(initial_state)
